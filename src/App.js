d996c182e5ab54e1a745ace568eeb5ee202171f2
Sara L <wwwsaramilk@gmail.com>
2022-05-25 16:17:38 -0300

    adionando novos projetos

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import Footer from './components/Footer';
 
 
 const arrayPro = [{
-  img: "images/steven.jpg",
-  title: "game memory",
-  desc: "this is my first try at creating a game",
-  ref1: "https://sarawwx.github.io/game-memory-test/",
-  ref2: "https://github.com/sarawwx/game-memory-test"
+  img: "images/wordflix.jpg",
+  title: "wordflix",
+  desc: "this is my first try recreating netflix page",
+  ref1: "https://clone-netflix-swart.vercel.app/",
+  ref2: "https://github.com/sarawwx/clone-netflix"
   },
   {
-    img: "images/steven.jpg",
-    title: "game memory",
-    desc: "this is my first try at creating a game",
-    ref1: "https://sarawwx.github.io/game-memory-test/",
-    ref2: "https://github.com/sarawwx/game-memory-test"
+    img: "images/nature.jpg",
+    title: "nature landing page",
+    desc: "this is a landing page to practice CSS",
+    ref1: "https://nature-landing-page.vercel.app/",
+    ref2: "https://github.com/sarawwx/nature-landing-page"
   },
   {
-    img: "images/steven.jpg",
-    title: "game memory",
-    desc: "this is my first try at creating a game",
-    ref1: "https://sarawwx.github.io/game-memory-test/",
-    ref2: "https://github.com/sarawwx/game-memory-test"
+    img: "images/seasons.jpg",
+    title: "slideshow",
+    desc: "this is a seasons themed slide",
+    ref1: "https://slide-nine.vercel.app/",
+    ref2: "https://github.com/sarawwx/slide"
   }];
 
 function App() {
